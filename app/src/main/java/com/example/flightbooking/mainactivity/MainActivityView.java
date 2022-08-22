c0c1b6b133e26a116519420755c93084eec66f82
Acer <Acer@DESKTOP-K09QF87>
2022-08-22 23:09:32 +0200

    setFragment method in MainActivityView performs add/replace fragment

--- app/src/main/java/com/example/flightbooking/mainactivity/MainActivityView.java
+++ app/src/main/java/com/example/flightbooking/mainactivity/MainActivityView.java
@@ -4,8 +4,10 @@ import android.content.Context;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.flightbooking.MainActivity;
+import com.example.flightbooking.R;
 
 public class MainActivityView {
 
@@ -19,6 +21,16 @@ public class MainActivityView {
 
     public void setFragment(Fragment fragment){
         FragmentManager fm = this.ma.getSupportFragmentManager();
-
+        Fragment current_fragment = fm.findFragmentById(R.id.main_activity_fragment_container);
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.setReorderingAllowed(true);
+        if(current_fragment != null){
+            //Container has already a fragment
+            ft.add(R.id.main_activity_fragment_container,fragment,null);
+        }//if(current_fragment != null){
+        else{
+            ft.replace(R.id.main_activity_fragment_container,fragment,null);
+        }
+        ft.commit();
     }
 }
