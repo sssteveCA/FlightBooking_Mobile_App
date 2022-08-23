8997a55b2ec6df7af4426337c292863b84e4a5b5
Acer <Acer@DESKTOP-K09QF87>
2022-08-23 20:44:19 +0200

    Added MainMenuView & MainMenuModel in fragments.mainmenu

---
+++ app/src/main/java/com/example/flightbooking/fragments/mainmenu/MainMenuModel.java
@@ -0,0 +1,10 @@
+package com.example.flightbooking.fragments.mainmenu;
+
+public class MainMenuModel {
+
+    private boolean logged; //select menu to show(not logged menu or logged menu)
+
+    public boolean isLogged(){return this.logged;}
+
+    public void setLogged(boolean logged){this.logged = logged;}
+}
