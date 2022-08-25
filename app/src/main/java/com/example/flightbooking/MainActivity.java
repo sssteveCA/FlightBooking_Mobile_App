6b6837c8530360aa5e981a6c88b135fa6d9b462c
Acer <Acer@DESKTOP-K09QF87>
2022-08-25 17:07:34 +0200

    Using reflection in setFragments of MainActivity.java

--- app/src/main/java/com/example/flightbooking/MainActivity.java
+++ app/src/main/java/com/example/flightbooking/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.flightbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.widget.ProgressBar;
@@ -9,6 +10,9 @@ import com.example.flightbooking.fragments.mainmenu.notlogged.MainMenuNotLoggedF
 import com.example.flightbooking.mainactivity.MainActivityModel;
 import com.example.flightbooking.mainactivity.MainActivityView;
 import com.example.flightbooking.fragments.noconnection.NoConnectionFragment;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class MainActivity extends AppCompatActivity implements NoConnectionFragment.RetryListener, MainMenuNotLoggedFragment.OnMainMenuItemClick {
 
@@ -38,28 +42,41 @@ public class MainActivity extends AppCompatActivity implements NoConnectionFragm
      */
     public void setFragments(String label){
         boolean userLogged = this.mam.isUserLogged();
+         Fragment fragment;
         if(userLogged){
             //User is logged with its account
         }//if(userLogged){
-        {
+        else{
             this.mav.updateFragment(R.id.main_menu_fragment_container, new MainMenuNotLoggedFragment());
         }
-        boolean require_connection = this.mam.isConnectionRequired(label);
-        if(require_connection){
-            //For this item internet is required
-            boolean connected = this.mam.getConnectionStatus();
-            if(connected){
-
-            }//if(connected){
+        String fragment_pkg = this.mam.getMenuItemFragmentPackage(label);
+        try {
+            //Create fragment instance by string package
+            Class<?> fragment_clazz = Class.forName(fragment_pkg);
+            Constructor<?> frag_constr = fragment_clazz.getConstructor();
+            boolean require_connection = this.mam.isConnectionRequired(label);
+            if(require_connection){
+                //For this item internet is required
+                boolean connected = this.mam.getConnectionStatus();
+                if(connected){
+                    this.mav.updateFragment(R.id.main_activity_fragment_container, (Fragment)frag_constr.newInstance());
+                }//if(connected){
+                else{
+                    this.mav.updateFragment(R.id.main_activity_fragment_container, new NoConnectionFragment());
+                }
+            }//if(require_connection){
             else{
-                this.mav.updateFragment(R.id.main_activity_fragment_container, new NoConnectionFragment());
-            }
-        }//if(require_connection){
-        else{
-
-        }//else di if(require_connection){
-
-
+                this.mav.updateFragment(R.id.main_activity_fragment_container, (Fragment)frag_constr.newInstance());
+            }//else di if(require_connection){
+        } catch (ClassNotFoundException | NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
 
     }
 
