d36774dd7a8e21d032fd85d11b83132628a2f5f5
Acer <Acer@DESKTOP-K09QF87>
2022-08-25 19:22:33 +0200

    Added getters & setters in LoginFragmentView

--- app/src/main/java/com/example/flightbooking/fragments/login/LoginFragmentView.java
+++ app/src/main/java/com/example/flightbooking/fragments/login/LoginFragmentView.java
@@ -1,4 +1,36 @@
 package com.example.flightbooking.fragments.login;
 
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
 public class LoginFragmentView {
+    private EditText et_username;
+    private EditText et_password;
+    private CheckBox cb_show_pass;
+    private Button bt_login;
+    private Button bt_reset;
+
+    public LoginFragmentView(EditText et_username,EditText et_password,CheckBox cb_show_pass,Button bt_login,Button bt_reset){
+        this.et_username = et_username;
+        this.et_password = et_password;
+        this.cb_show_pass = cb_show_pass;
+        this.bt_login = bt_login;
+        this.bt_reset = bt_reset;
+    }
+
+    public EditText getEtUsername(){return this.et_username;}
+    public EditText getEtPassword(){return this.et_password;}
+    public CheckBox getCbShowPass(){return this.cb_show_pass;}
+    public Button getBtLogin(){return this.bt_login;}
+    public Button getBtReset(){return this.bt_reset;}
+
+    /**
+     * Clear all Login EditText values
+     */
+    public void resetAll(){
+        this.et_username.setText("");
+        this.et_password.setText("");
+    }
+
 }
