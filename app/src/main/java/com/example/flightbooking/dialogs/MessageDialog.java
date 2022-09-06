0e8bcd33724af76b24d600d37c564fff60f43e4b
Acer <Acer@DESKTOP-K09QF87>
2022-09-06 21:50:55 +0200

    Added setDialog method & button positive click listener in MessageDialog

--- app/src/main/java/com/example/flightbooking/dialogs/MessageDialog.java
+++ app/src/main/java/com/example/flightbooking/dialogs/MessageDialog.java
@@ -1,11 +1,39 @@
 package com.example.flightbooking.dialogs;
 
+import static android.content.DialogInterface.BUTTON_POSITIVE;
+
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 
-public class MessageDialog extends AlertDialog.Builder {
+//Create a Dialog with message that is destroyed when OK button is pressed
+public class MessageDialog extends AlertDialog.Builder implements DialogInterface.OnClickListener {
 
-    public MessageDialog(Context context) {
+    private String title;
+    private String message;
+
+    public MessageDialog(Context context, String title, String message) {
         super(context);
+        this.title = title;
+        this.message = message;
+        this.setDialog();
+    }
+
+    public String getTitle(){return this.title;}
+    public String getMessage(){return this.message;}
+
+    private void setDialog(){
+        this.setTitle(this.title);
+        this.setMessage(this.message);
+        this.setPositiveButton("OK",this);
+        AlertDialog ad = this.create();
+        ad.show();
+    }
+
+    //DialogInterface.OnClickListener
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+        if(i == BUTTON_POSITIVE)
+            dialogInterface.dismiss();
     }
 }
