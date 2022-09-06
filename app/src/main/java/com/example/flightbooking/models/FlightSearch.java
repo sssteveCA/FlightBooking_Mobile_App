3ab8d512466c649f7af6d11985b3a8ece0ab2ef3
Acer <Acer@DESKTOP-K09QF87>
2022-09-06 22:56:18 +0200

    Added models.FlightSearch

---
+++ app/src/main/java/com/example/flightbooking/models/FlightSearch.java
@@ -0,0 +1,18 @@
+package com.example.flightbooking.models;
+
+//This class is used by retrofit when user wants to get ticket info before the payment
+public class FlightSearch {
+    public String flight_type;
+    public String company_name;
+    public String from_country;
+    public String from_airport;
+    public String to_country;
+    public String to_airport;
+    public String oneway_date;
+    public String roundtrip_start_date;
+    public String roundtrip_end_date;
+    public Integer adults;
+    public Integer teenagers;
+    public Integer children;
+    public Integer newborns;
+}
