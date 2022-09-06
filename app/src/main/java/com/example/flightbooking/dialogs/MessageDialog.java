2f6f0a54f57dddad4b34b691e50698dda4ce2e75
Acer <Acer@DESKTOP-K09QF87>
2022-09-06 21:31:32 +0200

    Added dialogs.MessageDialog

---
+++ app/src/main/java/com/example/flightbooking/dialogs/MessageDialog.java
@@ -0,0 +1,11 @@
+package com.example.flightbooking.dialogs;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class MessageDialog extends AlertDialog.Builder {
+
+    public MessageDialog(Context context) {
+        super(context);
+    }
+}
