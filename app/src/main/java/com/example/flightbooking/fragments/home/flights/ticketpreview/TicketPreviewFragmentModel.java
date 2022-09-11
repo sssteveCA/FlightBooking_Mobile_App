8fe545109ef4bfa5787afa8cc4ddcbfd5ff03fb6
Acer <Acer@DESKTOP-K09QF87>
2022-09-11 17:24:02 +0200

    Added properties, constructor and getters to TicketPreviewFragmentModel

--- app/src/main/java/com/example/flightbooking/fragments/home/flights/ticketpreview/TicketPreviewFragmentModel.java
+++ app/src/main/java/com/example/flightbooking/fragments/home/flights/ticketpreview/TicketPreviewFragmentModel.java
@@ -1,4 +1,20 @@
 package com.example.flightbooking.fragments.home.flights.ticketpreview;
 
+import com.example.flightbooking.models.FlightInfo;
+
+import java.util.HashMap;
+
 public class TicketPreviewFragmentModel {
+
+    private FlightInfo fi;
+    private String flight_type;
+    private HashMap<String, HashMap<String, Object>> flights;
+
+    public TicketPreviewFragmentModel(FlightInfo fi){
+        this.fi = fi;
+        this.flight_type = fi.flightType;
+    }
+
+    public String getFlightType(){return this.flight_type;}
+    public HashMap<String, HashMap<String, Object>> getFlights(){return this.flights;}
 }
