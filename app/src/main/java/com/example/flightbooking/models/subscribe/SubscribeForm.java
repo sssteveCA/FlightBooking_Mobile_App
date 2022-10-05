a7aff783f04120bd29cb1d237b6839b5df3624b1
Acer <Acer@DESKTOP-K09QF87>
2022-10-05 19:39:13 +0200

    Added models.subscribe.SubscribeForm

---
+++ app/src/main/java/com/example/flightbooking/models/subscribe/SubscribeForm.java
@@ -0,0 +1,11 @@
+package com.example.flightbooking.models.subscribe;
+
+/**
+ * Subscribe form input values
+ */
+public class SubscribeForm {
+    public String name;
+    public String email;
+    public String password;
+    public String password_confirmation;
+}
