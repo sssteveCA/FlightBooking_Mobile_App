f04f2e762dfc08e27b0f091addacfaa3ed8fd8ae
Acer <Acer@DESKTOP-K09QF87>
2022-10-05 21:47:02 +0200

    Added fragments.subscribe.verifyaccount.VerifyAccountFragmentView

---
+++ app/src/main/java/com/example/flightbooking/fragments/subscribe/verifyaccount/VerifyAccountFragmentView.java
@@ -0,0 +1,14 @@
+package com.example.flightbooking.fragments.subscribe.verifyaccount;
+
+import android.widget.Button;
+
+public class VerifyAccountFragmentView {
+
+    private Button bt_back;
+
+    public VerifyAccountFragmentView(Button bt_back){
+        this.bt_back = bt_back;
+    }
+
+    public Button getBtBack(){return this.bt_back;}
+}
