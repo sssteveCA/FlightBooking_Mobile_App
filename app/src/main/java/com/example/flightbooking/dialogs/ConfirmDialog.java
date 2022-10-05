c184ed90e6bb909a92bb683d036ec666d0bd08f4
Acer <Acer@DESKTOP-K09QF87>
2022-10-05 18:27:11 +0200

    Added onYesClick & onNoClick methods in ConfirmDialog class

--- app/src/main/java/com/example/flightbooking/dialogs/ConfirmDialog.java
+++ app/src/main/java/com/example/flightbooking/dialogs/ConfirmDialog.java
@@ -2,9 +2,40 @@ package com.example.flightbooking.dialogs;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 
 public class ConfirmDialog  extends AlertDialog.Builder {
+
+    private String title;
+    private String message;
+
     public ConfirmDialog(Context context) {
         super(context);
+    }
+
+    public String getTitle(){ return this.title; }
+    public String getMessage(){ return this.message; }
+
+    private void setDialog(){
+        this.setTitle(this.title);
+        this.setMessage(this.title);
+        AlertDialog ad = this.create();
+        ad.show();
+    }
+
+    /**
+     * When user press "YES" button in confirm dialog
+     * @param click the listener
+     */
+    public void onYesClick(DialogInterface.OnClickListener click){
+        this.setPositiveButton("SÌ",click);
+    }
+
+    /**
+     * When user press "NO" button in confirm dialog
+     * @param click the listener
+     */
+    public void onNoClick(DialogInterface.OnClickListener click){
+        this.setNegativeButton("NO",click);
     }
 }
