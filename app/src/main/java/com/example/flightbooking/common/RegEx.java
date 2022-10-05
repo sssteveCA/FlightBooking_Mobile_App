c0ef1e22de9d30f7f6847ea94c39fdd158017765
Acer <Acer@DESKTOP-K09QF87>
2022-10-05 19:21:01 +0200

    Added validateData method in SubscribeFragmentModel class

--- app/src/main/java/com/example/flightbooking/common/RegEx.java
+++ app/src/main/java/com/example/flightbooking/common/RegEx.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 public class RegEx {
 
     public static final String PATTERN_DATE = "^\\d{4}-((0\\d)|(1[0-2]))((?<=01|03|05|07|08|10|12)-(0\\d|1\\d|2\\d|3[01])|(?<=02)-(0\\d|1\\d|2\\d)|(?<=04|06|09|11)-(0\\d|1\\d|2\\d|30))$";
+    public static final String PATTERN_EMAIL = "^[a-z][a-z0-9]{4,30}@([a-z]{3,20}\\.){1,5}[a-z]{2,10}$";
     public static final String PATTERN_NUMBER = "^(0|[1-9]\\d*)$";
 
     /**
--- app/src/main/java/com/example/flightbooking/fragments/subscribe/SubscribeFragmentModel.java
+++ app/src/main/java/com/example/flightbooking/fragments/subscribe/SubscribeFragmentModel.java
@@ -1,4 +1,31 @@
 package com.example.flightbooking.fragments.subscribe;
 
+import com.example.flightbooking.common.RegEx;
+
+import java.util.Map;
+
 public class SubscribeFragmentModel {
+
+    public SubscribeFragmentModel(){
+
+    }
+
+    /**
+     * Check if subscribe data are valid
+     * @param data the map with the subscribe data
+     * @return
+     */
+    public boolean validateData(Map<String, String> data){
+        String username = data.get("username");
+        if(username.equals("") || username.length() > 255)return false;
+        String email = data.get("email");
+        if(!RegEx.patternMatches(RegEx.PATTERN_EMAIL,email) || email.length() > 255)return false;
+        String confirmEmail = data.get("conf_email");
+        if(!confirmEmail.equals(email))return false;
+        String password = data.get("password");
+        if(password.length() < 8)return false;
+        String confirmPassword = data.get("conf_password");
+        if(!confirmPassword.equals(password))return false;
+        return true;
+    }
 }
