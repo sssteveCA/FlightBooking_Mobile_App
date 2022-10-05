39530038928df5380e317dae8227197f02193c20
Acer <Acer@DESKTOP-K09QF87>
2022-10-05 18:14:20 +0200

    Added dialogs.ConfirmDialog class

---
+++ app/src/main/java/com/example/flightbooking/dialogs/ConfirmDialog.java
@@ -0,0 +1,10 @@
+package com.example.flightbooking.dialogs;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class ConfirmDialog  extends AlertDialog.Builder {
+    public ConfirmDialog(Context context) {
+        super(context);
+    }
+}
