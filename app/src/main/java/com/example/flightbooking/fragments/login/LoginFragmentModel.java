b102a880e6c60799885c02b5698fa76c3aa22020
Acer <Acer@DESKTOP-K09QF87>
2022-10-07 20:15:47 +0200

    Added validateData method in LoginFragmentModel

--- app/src/main/java/com/example/flightbooking/fragments/login/LoginFragmentModel.java
+++ app/src/main/java/com/example/flightbooking/fragments/login/LoginFragmentModel.java
@@ -1,4 +1,23 @@
 package com.example.flightbooking.fragments.login;
 
+import java.util.Map;
+
 public class LoginFragmentModel {
+
+    public LoginFragmentModel(){
+
+    }
+
+    /**
+     * Check if login data are valid
+     * @param data the map with the login data
+     * @return
+     */
+    public boolean validateData(Map<String, String> data){
+        String username = data.get("username");
+        if(username.equals(""))return false;
+        String password = data.get("password");
+        if(password.equals(""))return false;
+        return true;
+    }
 }
