330efc41e309d207556ec7b21dcf18d74b503a5e
Acer <Acer@DESKTOP-K09QF87>
2022-10-09 18:12:25 +0200

    Added properties, constructor, getters & setters in MainMenuLoggedModel

--- app/src/main/java/com/example/flightbooking/fragments/mainmenu/logged/MainMenuLoggedModel.java
+++ app/src/main/java/com/example/flightbooking/fragments/mainmenu/logged/MainMenuLoggedModel.java
@@ -1,4 +1,21 @@
 package com.example.flightbooking.fragments.mainmenu.logged;
 
+import android.content.Context;
+import com.example.flightbooking.models.MenuItem;
+import java.util.ArrayList;
+
 public class MainMenuLoggedModel {
+    private Context ctx;
+    private ArrayList<MenuItem> menuItems;
+    private int menuStatus; //Status of menu (hidden or expaned)
+
+    public MainMenuLoggedModel(Context ctx){
+        this.ctx = ctx;
+        this.menuItems = new ArrayList<>();
+    }
+
+    public ArrayList<MenuItem> getMenuItems(){return this.menuItems;}
+    public int getMenuStatus(){return this.menuStatus;}
+
+    public void setMenuStatus(int menuStatus){this.menuStatus = menuStatus;}
 }
