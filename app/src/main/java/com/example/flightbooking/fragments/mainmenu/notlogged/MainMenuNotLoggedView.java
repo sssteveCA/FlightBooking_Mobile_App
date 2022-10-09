c35113bf02ac2f876a793871a737da84dc9caca8
Acer <Acer@DESKTOP-K09QF87>
2022-10-09 18:24:26 +0200

    MainMenuNotLoggedView extends now abstract class MenuView

--- app/src/main/java/com/example/flightbooking/fragments/mainmenu/notlogged/MainMenuNotLoggedView.java
+++ app/src/main/java/com/example/flightbooking/fragments/mainmenu/notlogged/MainMenuNotLoggedView.java
@@ -3,24 +3,15 @@ package com.example.flightbooking.fragments.mainmenu.notlogged;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.example.flightbooking.fragments.mainmenu.MenuView;
+
 import java.util.ArrayList;
 
-public class MainMenuNotLoggedView {
-
-    private Button showHide;
-    private ListView menuView;
+public class MainMenuNotLoggedView extends MenuView {
 
     public MainMenuNotLoggedView(ListView menuView, Button showHide){
-        this.menuView = menuView;
-        this.showHide = showHide;
+        super(menuView, showHide);
     }
-
-    public Button getShowHide(){return this.showHide;}
-    public ListView getMenu(){return this.menuView;}
-
-    public void setShowHide(Button show_hide){this.showHide = show_hide;}
-    public void setMenuView(ListView menuView){this.menuView = menuView;}
-
 
     public void setListView(ArrayList<String> items){
 
