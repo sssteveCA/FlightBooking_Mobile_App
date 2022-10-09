a26311a1fba7116ff22c5a911fa8f7f77103bcf2
Acer <Acer@DESKTOP-K09QF87>
2022-10-09 20:54:30 +0200

    Added fragments.mainmenu.logged.profile.MainMenuLoggedProfileAdapter

---
+++ app/src/main/java/com/example/flightbooking/fragments/mainmenu/logged/profile/MainMenuLoggedProfileAdapter.java
@@ -0,0 +1,77 @@
+package com.example.flightbooking.fragments.mainmenu.logged.profile;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import com.example.flightbooking.fragments.mainmenu.notlogged.MainMenuNotLoggedAdapter;
+import com.example.flightbooking.models.login.Auth;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class MainMenuLoggedProfileAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private List<String> elv_items;
+    private HashMap<String, List<String>> elv_containers;
+    private Auth auth;
+
+    public MainMenuLoggedProfileAdapter(Context context, List<String> elv_items, HashMap<String, List<String>> elv_containers, Auth auth){
+        this.context = context;
+        this.elv_items = elv_items;
+        this.elv_containers = elv_containers;
+        this.auth = auth;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return 0;
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return false;
+    }
+}
