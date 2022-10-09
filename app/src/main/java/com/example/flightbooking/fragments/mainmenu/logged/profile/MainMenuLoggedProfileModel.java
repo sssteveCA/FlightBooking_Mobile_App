78f444f6f23b41553e199d03bd46b89ffc5a85a6
Acer <Acer@DESKTOP-K09QF87>
2022-10-09 22:36:37 +0200

    Added ExpandableListView in fragment_main_menu_logged.xml

--- app/src/main/java/com/example/flightbooking/fragments/mainmenu/logged/profile/MainMenuLoggedProfileModel.java
+++ app/src/main/java/com/example/flightbooking/fragments/mainmenu/logged/profile/MainMenuLoggedProfileModel.java
@@ -1,7 +1,11 @@
 package com.example.flightbooking.fragments.mainmenu.logged.profile;
 
+import android.content.Context;
+
+import com.example.flightbooking.models.MenuItem;
 import com.example.flightbooking.models.login.Auth;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,13 +15,24 @@ public class MainMenuLoggedProfileModel {
             "Il mio profilo", "I miei voli", "Logout"
     };
 
-    private HashMap<String, List<String>> profile_submenu_items;
+    private HashMap<String, List<MenuItem>> profile_submenu_items;
     private Auth auth;
 
-    public MainMenuLoggedProfileModel(HashMap<String, List<String>> profile_submenu_items, Auth auth){
-        this.profile_submenu_items = profile_submenu_items;
+    public MainMenuLoggedProfileModel(Context ctx, Auth auth){
         this.auth = auth;
     }
 
-    public HashMap<String, List<String>> getProfileSubmenuItems(){return this.profile_submenu_items;}
+    public HashMap<String, List<MenuItem>> getProfileSubmenuItems(){return this.profile_submenu_items;}
+
+    /**
+     * Set the submenu profile items list for logged users
+     */
+    public void setMenu(){
+        List<MenuItem> elv_items = new ArrayList<>();
+        for(String label: MainMenuLoggedProfileModel.items){
+            MenuItem mi = new MenuItem(label);
+            elv_items.add(mi);
+        }
+        this.profile_submenu_items.put(this.auth.user.name,elv_items);
+    }
 }
