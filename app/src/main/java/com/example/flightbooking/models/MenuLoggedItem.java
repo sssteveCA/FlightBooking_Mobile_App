8c8d1a693d9edc701339d5de1da5433690277ea4
Acer <Acer@DESKTOP-K09QF87>
2022-10-09 13:08:42 +0200

    Added models.MenuLoggedItem

---
+++ app/src/main/java/com/example/flightbooking/models/MenuLoggedItem.java
@@ -0,0 +1,12 @@
+package com.example.flightbooking.models;
+
+public class MenuLoggedItem {
+
+    private String label;
+
+    public MenuLoggedItem(String label){
+        this.label = label;
+    }
+
+    public String getLabel(){return this.label;}
+}
