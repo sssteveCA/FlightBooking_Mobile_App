96e17ab36a7a022b70587eb8a174098d4d93814f
Acer <Acer@DESKTOP-K09QF87>
2022-10-09 22:22:23 +0200

    Added properties, constructor and getters in MainMenuLoggedProfileModel

--- app/src/main/java/com/example/flightbooking/fragments/mainmenu/logged/profile/MainMenuLoggedProfileModel.java
+++ app/src/main/java/com/example/flightbooking/fragments/mainmenu/logged/profile/MainMenuLoggedProfileModel.java
@@ -1,4 +1,23 @@
 package com.example.flightbooking.fragments.mainmenu.logged.profile;
 
+import com.example.flightbooking.models.login.Auth;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class MainMenuLoggedProfileModel {
+
+    private static final String[] items = {
+            "Il mio profilo", "I miei voli", "Logout"
+    };
+
+    private HashMap<String, List<String>> profile_submenu_items;
+    private Auth auth;
+
+    public MainMenuLoggedProfileModel(HashMap<String, List<String>> profile_submenu_items, Auth auth){
+        this.profile_submenu_items = profile_submenu_items;
+        this.auth = auth;
+    }
+
+    public HashMap<String, List<String>> getProfileSubmenuItems(){return this.profile_submenu_items;}
 }
