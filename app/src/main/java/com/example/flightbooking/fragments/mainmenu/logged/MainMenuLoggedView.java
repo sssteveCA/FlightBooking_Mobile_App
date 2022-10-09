4a759077aac1f348b761aeb22e907e7b3dd06124
Acer <Acer@DESKTOP-K09QF87>
2022-10-09 13:02:05 +0200

    Added properties, constructor, getters and setters in MainMenuLoggedView

--- app/src/main/java/com/example/flightbooking/fragments/mainmenu/logged/MainMenuLoggedView.java
+++ app/src/main/java/com/example/flightbooking/fragments/mainmenu/logged/MainMenuLoggedView.java
@@ -1,4 +1,28 @@
 package com.example.flightbooking.fragments.mainmenu.logged;
 
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 public class MainMenuLoggedView {
+
+    private Button showHide;
+    private ListView menuView;
+
+    public MainMenuLoggedView(ListView menuView, Button showHide){
+        this.menuView = menuView;
+        this.showHide = showHide;
+    }
+
+    public Button getShowHide(){return this.showHide;}
+    public ListView getMenu(){return this.menuView;}
+
+    public void setShowHide(Button show_hide){this.showHide = show_hide;}
+    public void setMenuView(ListView menuView){this.menuView = menuView;}
+
+
+    public void setListView(ArrayList<String> items){
+
+    }
 }
