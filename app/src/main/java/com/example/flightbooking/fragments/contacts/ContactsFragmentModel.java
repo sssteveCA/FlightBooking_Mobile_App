77b21c5470aca98697f85f3daadfaac87d7eeab9
Acer <Acer@DESKTOP-K09QF87>
2022-10-23 09:49:48 +0200

    Added validateData and setContacts methods in ContactsFragmentModel class

--- app/src/main/java/com/example/flightbooking/fragments/contacts/ContactsFragmentModel.java
+++ app/src/main/java/com/example/flightbooking/fragments/contacts/ContactsFragmentModel.java
@@ -3,6 +3,8 @@ package com.example.flightbooking.fragments.contacts;
 import com.example.flightbooking.models.requests.contacts.Contacts;
 import com.example.flightbooking.models.response.Message;
 
+import java.util.Map;
+
 public class ContactsFragmentModel {
 
     public interface ContactsResponse{
@@ -30,5 +32,36 @@ public class ContactsFragmentModel {
 
     public void contactsRequest(){
 
+    }
+
+    /**
+     * Check if contacts data are valid
+     * @param data the map with the contacts data
+     * @return true if the data format is valid
+     */
+    private boolean validateData(Map<String, String> data){
+        String name = data.get("name");
+        if(name.equals(""))return false;
+        String email = data.get("email");
+        if(name.equals(""))return false;
+        String subject = data.get("subject");
+        if(subject.equals(""))return false;
+        String message = data.get("message");
+        if(message.equals(""))return false;
+        this.contacts = this.setContacts(name,email,subject,message);
+        return true;
+    }
+
+    /**
+     * Set the body class for contacts request
+     * @return a Contacts instance with the contacts data
+     */
+    private Contacts setContacts(String name, String email, String subject, String message){
+        Contacts contacts = new Contacts();
+        contacts.name = name;
+        contacts.email = email;
+        contacts.subject = subject;
+        contacts.message = message;
+        return contacts;
     }
 }
