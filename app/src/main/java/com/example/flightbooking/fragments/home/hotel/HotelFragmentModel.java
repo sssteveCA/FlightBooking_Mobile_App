c36dff9c06e2c39fd2c46c5cd048d36b399e3df4
Acer <Acer@DESKTOP-K09QF87>
2022-11-01 22:04:14 +0100

    Added getCountries method in HotelFragmentModel

--- app/src/main/java/com/example/flightbooking/fragments/home/hotel/HotelFragmentModel.java
+++ app/src/main/java/com/example/flightbooking/fragments/home/hotel/HotelFragmentModel.java
@@ -4,7 +4,12 @@ import android.content.Context;
 
 import com.example.flightbooking.common.Connection;
 
+import java.io.IOException;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HotelFragmentModel {
 
@@ -35,12 +40,40 @@ public class HotelFragmentModel {
         this.hfc = new HotelFragmentClient();
     }
 
-    /***
-     *
+    /**
      *  Check if phone is connected to internet
      * @return boolean
      */
     public boolean getConnectionStatus(){
         return Connection.checkInternet(this.context);
+    }
+
+    /**
+     * Perform the HTTP request to get the hotel countries list
+     */
+    public void getCountries(GetCountries gc){
+        this.hfc.getHfi().countries().enqueue(new Callback<List<String>>() {
+            @Override
+            public void onResponse(Call<List<String>> call, Response<List<String>> response) {
+                if(response.isSuccessful()){
+                    List<String> countries = response.body();
+                    gc.countriesResponse(countries);
+                }
+                else{
+                    String errorBody = "";
+                    try {
+                        errorBody = response.errorBody().string();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }finally {
+                        gc.countriesError(errorBody);
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<List<String>> call, Throwable t) {
+                gc.countriesError(t.getMessage());
+            }
+        });
     }
 }
