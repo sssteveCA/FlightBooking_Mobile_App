65e47b7769dc580733d5e6cd1776a4908ccd743b
Acer <Acer@DESKTOP-K09QF87>
2022-11-01 21:45:51 +0100

    Added properties, constructor and getters to HotelFragmentClient

--- app/src/main/java/com/example/flightbooking/fragments/home/hotel/HotelFragmentClient.java
+++ app/src/main/java/com/example/flightbooking/fragments/home/hotel/HotelFragmentClient.java
@@ -1,4 +1,26 @@
 package com.example.flightbooking.fragments.home.hotel;
 
+import com.example.flightbooking.interfaces.Globals;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class HotelFragmentClient {
+
+    public interface HotelFragmentInterface{
+
+    }
+
+    private Retrofit retrofit;
+    private HotelFragmentInterface hfi;
+
+    public HotelFragmentClient(){
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl(Globals.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public HotelFragmentInterface getHfi(){return this.hfi;}
+
 }
