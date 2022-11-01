8ee8188329b9a90b609287f783433cd223e08a01
Acer <Acer@DESKTOP-K09QF87>
2022-11-01 21:52:50 +0100

    Added abstract methods to HotelFragmentInterface interface in HotelFragmentClient

--- app/src/main/java/com/example/flightbooking/fragments/home/hotel/HotelFragmentClient.java
+++ app/src/main/java/com/example/flightbooking/fragments/home/hotel/HotelFragmentClient.java
@@ -1,14 +1,28 @@
 package com.example.flightbooking.fragments.home.hotel;
 
+import android.provider.Settings;
+
 import com.example.flightbooking.interfaces.Globals;
 
+import java.util.List;
+
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public class HotelFragmentClient {
 
     public interface HotelFragmentInterface{
+        @GET(Globals.API_ROUTES_PREFIX+"/hotelcountries")
+        Call<List<String>> countries();
 
+        @GET(Globals.API_ROUTES_PREFIX+"/hotelcities")
+        Call<List<String>> cities(@Query("country")String country);
+
+        @GET(Globals.API_ROUTES_PREFIX+"/hotelsearch")
+        Call<List<String>> hotels(@Query("country")String country,@Query("city")String city);
     }
 
     private Retrofit retrofit;
