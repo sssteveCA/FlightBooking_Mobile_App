5a1d2a545314cd50992ce1cea8d5893459e35999
Acer <Acer@DESKTOP-K09QF87>
2022-11-16 22:13:21 +0100

    Added getHotelsOfCity method to HotelFragmentModel

--- app/src/main/java/com/example/flightbooking/fragments/home/hotel/HotelFragmentModel.java
+++ app/src/main/java/com/example/flightbooking/fragments/home/hotel/HotelFragmentModel.java
@@ -154,15 +154,13 @@ public class HotelFragmentModel {
      */
     public LinkedList<String> getHotelCitiesOfCountry(String country){
         LinkedList<String> cities = new LinkedList<>();
-        if(!this.getHotelsCountries().isEmpty()){
-            if(this.hotelsInfo.has(country)){
-                JsonObject joCities = this.hotelsInfo.getAsJsonObject(country);
-                Set<Map.Entry<String, JsonElement>> entries = joCities.entrySet();
-                for(Map.Entry<String, JsonElement> entry: entries){
-                    cities.add(entry.getKey());
-                }
-            }//if(this.hotelsInfo.has(country)){
-        }//if(!this.getHotelsCountries().isEmpty()){
+        if(this.hotelsInfo.has(country)){
+            JsonObject joCities = this.hotelsInfo.getAsJsonObject(country);
+            Set<Map.Entry<String, JsonElement>> entries = joCities.entrySet();
+            for(Map.Entry<String, JsonElement> entry: entries){
+                cities.add(entry.getKey());
+            }
+        }//if(this.hotelsInfo.has(country)){
         return cities;
     }
 
@@ -179,6 +177,27 @@ public class HotelFragmentModel {
             }
         }//if(this.hotelsInfo != null){
         return countries;
+    }
+
+    /**
+     * Get a list of bookable hotels that are in a particular city
+     * @param country the country to search the hotels
+     * @param city the city to search the hotels
+     * @return
+     */
+    public LinkedList<String> getHotelsOfCity(String country, String city){
+        LinkedList<String> hotels = new LinkedList<>();
+        if(this.hotelsInfo.has(country)){
+            JsonObject joCities = this.hotelsInfo.getAsJsonObject(country);
+            if(joCities.has(city)){
+                JsonObject joHotels = joCities.getAsJsonObject(city);
+                Set<Map.Entry<String, JsonElement>> entries = joHotels.entrySet();
+                for(Map.Entry<String, JsonElement> entry: entries){
+                    hotels.add(entry.getKey());
+                }
+            }//if(joCities.has(city)){
+        }//if(this.hotelsInfo.has(country)){
+        return hotels;
     }
 
     /**
