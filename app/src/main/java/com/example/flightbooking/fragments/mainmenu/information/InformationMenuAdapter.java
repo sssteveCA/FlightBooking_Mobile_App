c82dff752f7e9696df4c6bbf21ed36b7b2d83863
stepp <stepp@mail.com>
2022-12-30 19:43:34 +0100

    Added res/layout/row_information_elv_title.xml

--- app/src/main/java/com/example/flightbooking/fragments/mainmenu/information/InformationMenuAdapter.java
+++ app/src/main/java/com/example/flightbooking/fragments/mainmenu/information/InformationMenuAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flightbooking.fragments.mainmenu.information;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
@@ -13,29 +14,33 @@ import java.util.List;
 public class InformationMenuAdapter  extends BaseExpandableListAdapter {
 
     private Context context;
-    private List<String> inf_items;
-    private HashMap<String, List<MenuItem>> inf_containers;
+    private List<String> elv_items;
+    private HashMap<String, List<MenuItem>> elv_containers;
 
-
+    public InformationMenuAdapter(Context context, List<String> inf_items, HashMap<String, List<MenuItem>> inf_containers){
+        this.context = context;
+        this.elv_items = inf_items;
+        this.elv_containers = inf_containers;
+    }
 
     @Override
     public int getGroupCount() {
-        return this.inf_items.size();
+        return this.elv_items.size();
     }
 
     @Override
     public int getChildrenCount(int i) {
-        return this.inf_containers.get(this.inf_items.get(i)).size();
+        return this.elv_containers.get(this.elv_items.get(i)).size();
     }
 
     @Override
     public Object getGroup(int i) {
-        return this.inf_items.get(i);
+        return this.elv_items.get(i);
     }
 
     @Override
     public Object getChild(int i, int i1) {
-        return this.inf_containers.get(this.inf_items.get(i)).get(i1);
+        return this.elv_containers.get(this.elv_items.get(i)).get(i1);
     }
 
     @Override
@@ -55,6 +60,11 @@ public class InformationMenuAdapter  extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        String listTitle = (String) getGroup(i);
+        if(view == null){
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = layoutInflater.inflate(0,null);
+        }//if(view == null){
         return null;
     }
 
