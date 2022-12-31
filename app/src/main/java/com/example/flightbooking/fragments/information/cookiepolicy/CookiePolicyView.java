7a635079185641636df4df7f6a59033e1c936700
stepp <stepp@mail.com>
2022-12-31 19:09:42 +0100

    Added fragments.information.cookiepolicy.CookiePolicyView

---
+++ app/src/main/java/com/example/flightbooking/fragments/information/cookiepolicy/CookiePolicyView.java
@@ -0,0 +1,14 @@
+package com.example.flightbooking.fragments.information.cookiepolicy;
+
+import android.widget.Button;
+
+public class CookiePolicyView {
+
+    private Button bt_back;
+
+    public CookiePolicyView(Button bt_back){
+        this.bt_back = bt_back;
+    }
+
+    public Button getBtBack(){return this.bt_back;}
+}
