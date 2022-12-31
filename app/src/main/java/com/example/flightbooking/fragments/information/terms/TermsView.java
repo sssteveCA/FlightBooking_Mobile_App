7be253bd87b0ede0d75086d83000151336804c0c
stepp <stepp@mail.com>
2022-12-31 19:11:45 +0100

    Added fragments.information.terms.TermsView

---
+++ app/src/main/java/com/example/flightbooking/fragments/information/terms/TermsView.java
@@ -0,0 +1,14 @@
+package com.example.flightbooking.fragments.information.terms;
+
+import android.widget.Button;
+
+public class TermsView {
+
+    private Button bt_back;
+
+    public TermsView(Button bt_back){
+        this.bt_back = bt_back;
+    }
+
+    public Button getBtBack(){return this.bt_back;}
+}
