6f93c3a4489567128ed4b9224a16f7e7e57b3fd3
stepp <stepp@mail.com>
2022-12-31 19:10:52 +0100

    Added fragments.information.privacypolicy.PrivacyPolicyView

---
+++ app/src/main/java/com/example/flightbooking/fragments/information/privacypolicy/PrivacyPolicyView.java
@@ -0,0 +1,14 @@
+package com.example.flightbooking.fragments.information.privacypolicy;
+
+import android.widget.Button;
+
+public class PrivacyPolicyView {
+
+    private Button bt_back;
+
+    public PrivacyPolicyView(Button bt_back){
+        this.bt_back = bt_back;
+    }
+
+    public Button getBtBack(){return this.bt_back;}
+}
