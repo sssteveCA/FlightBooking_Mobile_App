6f1f66112572b41b47e1f6618855fd058460025d
stepp <stepp@mail.com>
2023-01-07 17:07:10 +0100

    Added models.response.news.Post

---
+++ app/src/main/java/com/example/flightbooking/models/response/news/Post.java
@@ -0,0 +1,34 @@
+package com.example.flightbooking.models.response.news;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Post {
+
+    @SerializedName("id")
+    @Expose
+    public Integer id;
+    @SerializedName("title")
+    @Expose
+    public String title;
+    @SerializedName("excerpt")
+    @Expose
+    public String excerpt;
+    @SerializedName("content")
+    @Expose
+    public String content;
+    @SerializedName("categories")
+    @Expose
+    public List<String> categories = null;
+    @SerializedName("tags")
+    @Expose
+    public List<String> tags = null;
+    @SerializedName("created_at")
+    @Expose
+    public String createdAt;
+    @SerializedName("updated_at")
+    @Expose
+    public String updatedAt;
+}
