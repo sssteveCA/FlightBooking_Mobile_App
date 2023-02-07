d80483bd0be7647842e9277941a52a515b5c8f3d
stepp <stepp@mail.com>
2023-02-07 21:25:44 +0100

    Added ViewHolder inner class to EventsFragmentAdapter

--- app/src/main/java/com/example/flightbooking/fragments/home/events/EventsFragmentAdapter.java
+++ app/src/main/java/com/example/flightbooking/fragments/home/events/EventsFragmentAdapter.java
@@ -2,6 +2,8 @@ package com.example.flightbooking.fragments.home.events;
 
 import android.content.Context;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
@@ -12,5 +14,13 @@ import java.util.List;
 public class EventsFragmentAdapter extends ArrayAdapter<FlightEvent> {
     public EventsFragmentAdapter(@NonNull Context context, int resource, @NonNull List<FlightEvent> objects) {
         super(context, resource, objects);
+    }
+
+    private class ViewHolder{
+        public ImageView image;
+        public TextView name;
+        public TextView date;
+        public TextView city;
+        public TextView price;
     }
 }
