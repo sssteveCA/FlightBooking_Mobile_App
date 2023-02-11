63d038bf617f2c08ee72ad687827e151ceed3784
stepp <stepp@mail.com>
2023-02-11 23:10:18 +0100

    Implemented View.OnClickListener interface to SingleEventFragment

--- app/src/main/java/com/example/flightbooking/fragments/home/events/singleevent/SingleEventFragment.java
+++ app/src/main/java/com/example/flightbooking/fragments/home/events/singleevent/SingleEventFragment.java
@@ -1,5 +1,6 @@
 package com.example.flightbooking.fragments.home.events.singleevent;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,13 +10,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.flightbooking.R;
+import com.example.flightbooking.models.response.flightevents.FlightEvent;
+
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SingleEventFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SingleEventFragment extends Fragment {
+public class SingleEventFragment extends Fragment implements View.OnClickListener {
+
+    private FlightEvent fe;
+    private Bitmap bitmap;
+    private SingleEventFragmentView sefv;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -54,6 +62,8 @@ public class SingleEventFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            this.fe = (FlightEvent) getArguments().getSerializable("flightEvent");
+            this.bitmap = getArguments().getParcelable("image");
         }
     }
 
@@ -61,6 +71,15 @@ public class SingleEventFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_single_event, container, false);
+        View v = inflater.inflate(R.layout.fragment_single_event, container, false);
+        HashMap<String, View> items = (HashMap<String, View>) SingleEventFragmentMethods.setViews(v);
+        this.sefv = new SingleEventFragmentView(items);
+        return v;
+    }
+
+    //View.OnClickListener
+    @Override
+    public void onClick(View view) {
+
     }
 }
