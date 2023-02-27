c045a8c4e422b745d442006d250b322a28d75cb1
stepp <stepp@mail.com>
2023-02-27 19:45:21 +0100

    Added activities.close.CloseActivity

---
+++ app/src/main/java/com/example/flightbooking/activities/close/CloseActivity.java
@@ -0,0 +1,14 @@
+package com.example.flightbooking.activities.close;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CloseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+    }
+}
