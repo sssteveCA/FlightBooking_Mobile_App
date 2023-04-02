6df52b89b0400cf2eedffbf000dda09eb52995d5
stepp <stepp@mail.com>
2023-04-02 17:56:17 +0200

    Added fragments.home.flights.ticketpreview.TicketPreviewFragmentClient

---
+++ app/src/main/java/com/example/flightbooking/fragments/home/flights/ticketpreview/TicketPreviewFragmentClient.java
@@ -0,0 +1,31 @@
+package com.example.flightbooking.fragments.home.flights.ticketpreview;
+
+import com.example.flightbooking.interfaces.Globals;
+import com.example.flightbooking.models.requests.flights.BookFlight;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public class TicketPreviewFragmentClient {
+
+    public interface TicketPreviewFragmentInterface{
+        @POST(Globals.API_ROUTES_PREFIX+"/bookflight")
+        Call<String> bookflight(@Body BookFlight bk);
+    }
+
+    private Retrofit retrofit;
+    private TicketPreviewFragmentInterface tpfi;
+
+    public TicketPreviewFragmentClient(){
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl(Globals.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        this.tpfi = this.retrofit.create(TicketPreviewFragmentInterface.class);
+    }
+
+    public TicketPreviewFragmentInterface getTpfi(){ return this.tpfi; }
+}
