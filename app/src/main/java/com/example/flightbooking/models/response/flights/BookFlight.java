a65e4b3888ca1c561fca92bbdfa9edfd4c2c0e1b
stepp <stepp@mail.com>
2023-04-02 18:11:22 +0200

    Added models.response.flights.BookFlight

---
+++ app/src/main/java/com/example/flightbooking/models/response/flights/BookFlight.java
@@ -0,0 +1,44 @@
+package com.example.flightbooking.models.response.flights;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Response body for /api/bookflight route
+ */
+public class BookFlight {
+
+    @SerializedName("done")
+    @Expose
+    public Boolean done;
+    @SerializedName("status")
+    @Expose
+    public String status;
+    @SerializedName("message")
+    @Expose
+    public String message;
+    @SerializedName("inserted")
+    @Expose
+    public Boolean inserted;
+    @SerializedName("flights_number")
+    @Expose
+    public Integer flightsNumber;
+    @SerializedName("flights_array")
+    @Expose
+    public List<FlightsArray> flightsArray;
+
+    class FlightsArray {
+        @SerializedName("id")
+        @Expose
+        public Integer id;
+        @SerializedName("name")
+        @Expose
+        public String name;
+        @SerializedName("flight_price")
+        @Expose
+        public Double flightPrice;
+    }
+
+}
